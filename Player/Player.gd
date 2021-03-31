ee7cfe633ab21166e78ed24d505af7aa93acb185
Nar Cuenca <lunar.jay.cuenca@benyameenissaoutlook.onmicrosoft.com>
2021-03-31 21:14:25 +0800

    added project files

---
+++ Player/Player.gd
@@ -0,0 +1,62 @@
+extends KinematicBody2D
+
+const ACCELERATION = 500
+const FRICTION = 500
+const MAX_SPEED = 80
+
+enum {
+	ATTACK,
+	MOVE,
+	ROLL
+}
+
+var state = MOVE
+var velocity = Vector2.ZERO
+
+onready var animationPlayer = $AnimationPlayer
+onready var animationTree = $AnimationTree
+onready var animationState = animationTree.get("parameters/playback")
+
+func _ready():
+	animationTree.active = true
+
+func _process(delta):
+	match state:
+		MOVE:
+			move_state(delta)
+		
+		ROLL:
+			pass
+			
+		ATTACK:
+			attack_state(delta)
+
+func move_state(delta):
+	var input_vector = Vector2.ZERO
+	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	input_vector = input_vector.normalized()
+	
+	if input_vector != Vector2.ZERO:
+		animationTree.set("parameters/Idle/blend_position", input_vector)
+		animationTree.set("parameters/Run/blend_position", input_vector)
+		animationTree.set("parameters/Attack/blend_position", input_vector)
+		animationState.travel("Run")
+		
+		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
+	else:
+		animationState.travel("Idle")
+		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
+	
+	velocity = move_and_slide(velocity)
+	
+	if (Input.is_action_just_pressed("attack")):
+		state = ATTACK
+
+func attack_state(delta):
+	velocity = Vector2.ZERO
+	
+	animationState.travel("Attack")
+
+func attack_animation_done():
+	state = MOVE
